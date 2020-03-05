f9bcb34e911284cdfd5d02f9f8be6efb0c625cb9
santhosh <santhoshcusat17@gmail.com>
2020-03-05 19:28:03 +0530

    Numbers and strings

---
+++ 01-numbers&strings.py
@@ -0,0 +1,67 @@
+"""
+    Topic: Numbers & Strings
+    Author: YSK-PYTHON
+    python : 3.5.2
+
+"""
+
+# ================= numbers =======================
+x = 10
+
+y = 15
+
+# Float
+
+a = 5.2
+
+b = 6.3
+
+age = 12
+
+# Arithmetic operator's
+
+add = 5 + 6  # addition
+print('Addition : ', add)
+
+sub = 5 - 3  # subtraction
+print('subtraction:', sub)
+
+mul = 54 * 53  # multiplication
+print('Multiplication: ', mul)
+
+div = 10/2  # classic division (float)
+print('Division:', div)
+
+fdiv = 15//2  # Floor division (discards fractional part)
+print('Floor division:', fdiv)
+
+rem = 5%2  # reminder
+print('Reminder', rem)
+
+
+print(a*b)
+
+
+# ==================== Strings ========================
+
+# Strings
+
+name = 'santhosh'
+print('Name: ',name)
+
+city = 'Kochi'
+print('City', city)
+
+print(name[0])
+print(name[0:5])
+print(name[-1])
+
+print(name * 3)
+
+print(name + city)
+
+
+
+
+
+
