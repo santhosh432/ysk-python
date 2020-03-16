52a57b1d9fbc377514421a02b9ad6183e722ec6a
santhosh <santhoshcusat17@gmail.com>
2020-03-16 18:29:53 +0530

    Data structures -list - ex -1

---
+++ 03-datastructures_ex-1.py
@@ -0,0 +1,13 @@
+
+"""
+    Topic: Data Structure's
+    Author: YSK-PYTHON
+    python : 3.5.2
+
+"""
+
+# 1.Create a list with random numbers and find length of list?
+# 2.Create a list with some number sand check an item exists in the list or not?
+# 3.Remove the last and first item in the list.
+# 4.Write a programme finding a duplicate elements in the list.
+# 5.Write a programme to get a unique number sin a list .
