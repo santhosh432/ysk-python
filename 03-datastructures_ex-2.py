a17b5a90e09f00afe151b19914ba08168286a439
santhosh <santhoshcusat17@gmail.com>
2020-03-18 19:31:21 +0530

    Ex 2 on list and dict

---
+++ 03-datastructures_ex-2.py
@@ -0,0 +1,35 @@
+
+"""
+    Topic: Data Structure's
+    Author: YSK-PYTHON
+    python : 3.5.2
+
+"""
+
+# 1.Create a list with random numbers and find length of list?
+# 2.Create a list with some number sand check an item exists in the list or not?
+# 3.Remove the last and first item in the list.
+# 4.Write a programme finding a duplicate elements in the list.
+# 5.Write a programme to get a unique numbers in a list .
+
+# 1. scores = {'palyer1': [23,56,8,98,100], 'player2': [89,25,6,3,6], 'player3': [0,2,5,6,9]} ,
+#  form the given dictionary find the total score of eac player , the output will in the given format
+# total_scores = {'player1' : 25, 'player2': 2, 'player3': 25}
+
+# 2. Find the average of all players in the above scores dictionary object.
+
+# 3 my_list = [100, 200, 100, 200,300,100,100,200]
+# write a programe to number of times repeated in the list.
+# {100 : 3, 200:3, 300:1}
+
+# 4.Write a programme to display in the given format .
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# 5. my_numbers = [8,9,6,56,94,3,6,5,63,1,2,5,87,8,33,45,7,775,4845,85,96,4]
+# create a new list with numbers which all are divisible by both 3 and 5 .
+# create a new list with numbers which all are divisible by either 5 or 7.
