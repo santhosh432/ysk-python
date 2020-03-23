b8edea396163f1eeff8f0b10c2096efe5f0691e0
santhosh <santhoshcusat17@gmail.com>
2020-03-23 21:38:52 +0530

    exceptions ...

---
+++ 07-map_reduce_filter_ex.py
@@ -0,0 +1,21 @@
+# exercise
+
+# 1 . Write a python programme to find out sum of each list objects in the given list.
+
+my_marks = [[60,78,56,96,45], [45,69,85,67,49], [54,89,63,75]]
+
+# 2. Write a python prgrame to get fullname of each dict object
+
+my_name = [{'first_name': 'sachin', 'last_name': 'Tendulkar'},
+           {'first_name': 'virat', 'last_name':'kohli'},
+           {'first_name':'MS', 'last_name':'Dhoni'}]
+
+# 3. Write a python programme to get a list of list items which contains at least one century in the give list
+
+my_scores = [[25,100,20,30], [56,89,65,12], [54,105,103]]
+
+# 4. Find the sum and multiplication of all elements in the given tuple.
+
+my_items =(2,3,6,9,6,7)
+
+
